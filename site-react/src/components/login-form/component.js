98ddd56d0e52bb785ea5a83f27fccfc6073fe688
eugen1337 <evgenijkurakin478@gmail.com>
2024-03-02 17:36:29 +0700

    global context

--- site-react/src/components/login-form/component.js
+++ site-react/src/components/login-form/component.js
@@ -7,18 +7,11 @@ import Manager from "../../manager.js";
 import "./style.css";
 
 export default function LoginForm(props) {
-<<<<<<< Updated upstream
-    const [username, setUsername] = useState("");
-    const [passwd, setPasswd] = useState("");
-    const navigate = useNavigate();
-
-=======
     const navigate = useNavigate();
 
     let { login, setLogin } = useContext(GlobalContext);
     let { password, setPassword } = useContext(GlobalContext);
 
->>>>>>> Stashed changes
     const manager = new Manager();
 
     const handleLogin = (event) => {
@@ -27,37 +20,12 @@ export default function LoginForm(props) {
     const handlePassword = (event) => {
         setPassword(event.target.value);
     };
-<<<<<<< Updated upstream
-    const login = () => {
-=======
 
     const queryLogin = () => {
         manager.updateState("username", login);
         manager.updateState("password", password);
->>>>>>> Stashed changes
         manager.query("login");
 
-<<<<<<< Updated upstream
-    const checkState = (stateName, state) => {
-        switch (stateName) {
-            case "login":
-                if (state.status === "OK" && state.token !== "BAD") {
-                    setUsername(username);
-                    navigate("/tasks");
-                } else alert("Неправильный логин или пароль!");
-                break;
-            case "username":
-                setUsername(state);
-                break;
-            case "password":
-                setPasswd(state);
-                break;
-        }
-    };
-    const unsubscribe = () => {
-        for (let i = 0; i < 3; i++) manager.unsubscribe(checkState);
-    };
-=======
         if (manager.getState("logged")) {
             navigate("/tasks");
         }
@@ -67,7 +35,6 @@ export default function LoginForm(props) {
         manager.updateState("username", login);
         manager.updateState("password", password);
         manager.query("register");
->>>>>>> Stashed changes
 
         if (manager.getState("logged")) {
             navigate("/tasks");
@@ -112,10 +79,7 @@ export default function LoginForm(props) {
                             id="register-btn"
                             type="button"
                             value="register"
-<<<<<<< Updated upstream
-=======
                             onClick={queryRegister}
->>>>>>> Stashed changes
                         />
                     </div>
                 </form>
